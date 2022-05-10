72faffc5a695845168052471e1a44d458c3f003b
wr3dny <you@example.com>
2022-05-10 21:41:42 +0200

    Carry on

--- 01_ Intro/bmi.py
+++ 01_ Intro/bmi.py
@@ -2,4 +2,4 @@ weight = float(input('Podaj swoją wagę w kg: '))
 height = float(input('Podaj swój wzrost w m: '))
 bmi = weight / height ** 2
 
-print('Twoje BMI wynosi:', round(bmi,2))
+print('Twoje BMI wynosi: ', round(bmi,2))
