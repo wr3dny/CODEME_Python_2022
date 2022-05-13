3f20cf0dd2fb2aa262023694103f0a79f5635edf
wr3dny <you@example.com>
2022-05-13 14:28:01 +0200

    First two - updated

--- 02_Zmienne_typy/numbers_1_fuel_usage.py
+++ 02_Zmienne_typy/numbers_1_fuel_usage.py
@@ -1,4 +1,22 @@
+print('#1 ze "sztywnymi" danymi\n')
 usage_fuel = 6.4
 distanse = 120
-price_of_fuel =
+price_of_fuel = 5.04
 
+trip_fuel_usage = distanse / 100 * usage_fuel
+
+print(trip_fuel_usage, 'litrów')
+
+price_of_trip = trip_fuel_usage * price_of_fuel
+
+# z możliwością wyboru danych - zadanie domowe
+
+print('\n#2\n')
+
+average_fuel_usage = float(input('Give Your vehical average fuel usage: '))
+predicted_distance = float(input('Whats Your predicted distance: '))
+fuel_price = float(input('How much for gas: '))
+
+user_cost_of_traveling = predicted_distance / 100 * average_fuel_usage * fuel_price
+
+print('This trip will cost You', round(user_cost_of_traveling,2), 'coins')
